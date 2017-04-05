e218ce30294ee27b2d7987e88d03fa28358a9fb2
yashrajpchavda <yashrajpchavda@gmail.com>
2017-04-05 15:24:36 +0530

    Added react-app project to the repository.

---
+++ react-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+console.log( 'working' );
+ReactDOM.render(
+    <App />,
+    document.querySelector( '#app' )
+);
